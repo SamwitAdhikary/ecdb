bd9479aaf3976c894b6ca282af3446021f3c452a
Samwit Adhikary <samwitadhikary2@gmail.com>
2023-09-01 06:52:38 +0530

    Made Changes

--- database/settings.py
+++ database/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'database.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database',
+        'USER': 'default',
+        'PASSWORD': '0ri9oRDwCqGd',
+        'HOST': 'ep-damp-forest-81450295-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT': '',
     }
 }
 
